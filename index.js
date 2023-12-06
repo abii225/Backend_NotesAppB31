fb0f0d818728378c16e73389d09c376b445d7304
abii225 <abhayv225@gmail.com>
2023-12-06 19:07:05 +0530

    changes on cors

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const {connection}=require("./DB/db");
 const {userRouter}=require("./Controller/user.Route");
 const {notesRouter}=require("./Controller/note.Route");
 const cors=require("cors");
-
+app.use(cors());
 
 app.use(express.json());
-app.use(cors);
-
-
-app.use("/users",userRouter);
-app.use("/notes",notesRouter);
 
 app.get("/",(req,res)=>{
     res.send("homepage")
 })
 
+app.use("/users",userRouter);
+app.use("/notes",notesRouter);
+
+
+
 app.listen(8080,async()=>{
     try{
           await connection
           console.log("DB connected..")
-          console.log("server is running...http://127.0.0.1:8080")
+          console.log("server is running...http://localhost:8080")
     }catch(err){
         console.log(err);
     }
